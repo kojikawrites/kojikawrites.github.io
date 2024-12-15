8898d08e43158b13562eb09f00bede7f723eb3a2
hiive <a.rollings@hiive.com>
2024-12-14 23:34:54 -0500

    Added in dev mode for getPosts.ts

--- src/utils/getPosts.ts
+++ src/utils/getPosts.ts
@@ -1,15 +1,29 @@
 import siteConfig from "../assets/config/site_config.yml"
 
+const isDev = import.meta.env.DEV;
+
 export default function getPosts() {
     const posts = () => {
         switch (siteConfig.blog_path) {
             case "posts":
+                if (isDev) {
+                    return import.meta.glob(
+                        ['../assets/posts/**/*.{md,mdx}',
+                            '!../assets/posts/**/_drafts/**'],
+                        {eager: true});
+                }
                 return import.meta.glob(
                     ['../assets/posts/**/*.{md,mdx}',
                         '!../assets/posts/**/_drafts/**',
                         '!../assets/posts/**/_test/**'],
                     {eager: true});
             default:
+                if (isDev) {
+                    return import.meta.glob(
+                        ['../assets/blog/**/*.{md,mdx}',
+                            '!../assets/blog/**/_drafts/**'],
+                        {eager: true});
+                }
                 return import.meta.glob(
                     ['../assets/blog/**/*.{md,mdx}',
                         '!../assets/blog/**/_drafts/**',
