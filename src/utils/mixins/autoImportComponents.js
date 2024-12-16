7eb4eef4023caee862bc223570cc1842b3ddebd3
hiive <a.rollings@hiive.com>
2024-12-16 10:27:35 -0500

    Added a bunch of fonts. Tweaked intro and mission pages.

--- src/utils/mixins/autoImportComponents.js
+++ src/utils/mixins/autoImportComponents.js
@@ -3,6 +3,7 @@ import LightboxGallery from "../../components/gallery/LightboxGallery.astro";
 import LightboxImage from "../../components/gallery/LightboxImage.astro";
 import ContentWarning from "../../components/posts/ContentWarning.astro";
 import FormattedDate from "../../components/FormattedDate.astro";
+import HiiveLabsText from "../../components/custom/hiiveLabs/HiiveLabsText.astro";
 
 export const components = {
     GalleryEntry,
@@ -10,4 +11,5 @@ export const components = {
     LightboxImage,
     ContentWarning,
     FormattedDate,
+    HiiveLabsText
 };
