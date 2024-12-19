490014b7ba8c3c35cff72d60d969f34867cf3aea
hiive <a.rollings@hiive.com>
2024-12-19 16:16:01 -0500

    dynamic loading of bluesky.json based on site.

---
+++ src/utils/getBlueskyPostedJson.ts
@@ -0,0 +1,22 @@
+import {getSiteCode} from "./getSiteConfig";
+
+export async function getBlueskyPostedJson(): Promise<Record<string, any>> {
+    const site = getSiteCode();
+
+
+
+    const jsonGlobs = import.meta.glob<{
+        default: any
+    }>('/src/assets/_private/state/**/*.json');
+    const jsonKeys = Object.keys(jsonGlobs);
+    console.log('jsonKeys', jsonGlobs);
+    const matchingKey = jsonKeys.find(key => key.includes(site));
+    if (!matchingKey) {
+        console.warn(`No bluesky json found for ${site}`);
+        return null;
+    }
+    console.log(`Reading ${site} bluesky json...`);
+    const jsonValue = await jsonGlobs[matchingKey]();
+    console.log('jsonValue', jsonValue.default);
+    return jsonValue.default;
+}
