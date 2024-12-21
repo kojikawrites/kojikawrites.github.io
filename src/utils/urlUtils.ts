fb3ce152ec11ead302d86dbd2b6403ac51551f1e
hiive <a.rollings@hiive.com>
2024-12-21 00:05:56 -0500

    fix for html links

--- src/utils/urlUtils.ts
+++ src/utils/urlUtils.ts
@@ -3,15 +3,19 @@ import getPostData from "./getPostData";
 import {getSiteConfig} from "./getSiteConfig";
 const siteConfig = await getSiteConfig();
 
+export function cleanUrl(url: string) {
+    return url.replace(/\/\//g, '/').replace(/\.html$/i, '');
+}
+
 export default function buildPostHref(post, prefix='/') : string {
     const {  path } = getPostData(post);
-    const permalink = `${prefix}${siteConfig.blog.prefix}${path}`
-    console.log('permalink', permalink);
+    const permalink = cleanUrl(`${prefix}${siteConfig.blog.prefix}${path}`);
+    console.log('buildPostHref: permalink', permalink);
     return permalink;
 }
 
 export function buildPermalink(siteRoot: string, urlPathname:string) {
-    const permalink = `${siteRoot}${urlPathname}`;
-    console.log('permalink', permalink);
+    const permalink = cleanUrl(`${siteRoot}${urlPathname}`);
+    console.log('buildPermalink: permalink', permalink);
     return permalink;
 }
