28bf57c27b21853a304f57037ac2c3cdcb5d2547
hiive <a.rollings@hiive.net>
2024-12-20 22:35:27 -0500

    Tweaks for build issues with breadcrumbs. (#16)

--- src/utils/buildPostHref.ts
+++
@@ -1,9 +0,0 @@
-import getPostData from "./getPostData";
-
-import {getSiteConfig} from "./getSiteConfig";
-const siteConfig = await getSiteConfig();
-
-export default function buildPostHref(post, prefix='/') : string {
-    const {  path } = getPostData(post);
-    return `${prefix}${siteConfig.blog.prefix}${path}`
-}
