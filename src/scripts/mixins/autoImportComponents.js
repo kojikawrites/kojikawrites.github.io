1c2e22e51a4ac986f122015dba895e7d92d671a8
hiive <a.rollings@hiive.com>
2025-01-28 17:10:06 -0500

    Updated the BI2 post to include links to Steam/Wiki as appropriate using new SteamLink.astro and WikiLink.astro components.

--- src/scripts/mixins/autoImportComponents.js
+++ src/scripts/mixins/autoImportComponents.js
@@ -4,12 +4,15 @@ import LightboxImage from "../../components/gallery/LightboxImage.astro";
 import ContentWarning from "../../components/posts/ContentWarning.astro";
 import FormattedDate from "../../components/FormattedDate.astro";
 import HiiveLabs from "../../components/custom/hiivelabs/HiiveLabs.astro";
-
+import SteamLink from "../../components/links/SteamLink.astro";
+import WikiLink from "../../components/links/WikiLink.astro";
 export const components = {
     GalleryEntry,
     LightboxGallery,
     LightboxImage,
     ContentWarning,
     FormattedDate,
+    SteamLink,
+    WikiLink,
     HiiveLabsText: HiiveLabs
 };
