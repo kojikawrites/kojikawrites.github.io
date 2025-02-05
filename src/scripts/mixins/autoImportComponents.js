1e5605d29f7eb85c01ebfa2ddbb1513ddfbc069e
hiive <a.rollings@hiive.com>
2025-02-05 14:25:42 -0500

    Updated ml experiment post to include embedded video.

--- src/scripts/mixins/autoImportComponents.js
+++ src/scripts/mixins/autoImportComponents.js
@@ -1,6 +1,8 @@
 import GalleryImage from "../../components/gallery/GalleryImage.astro";
+// import GalleryVideo from "../../components/gallery/GalleryVideo.astro";
 import LightboxGallery from "../../components/gallery/LightboxGallery.astro";
 import LightboxImage from "../../components/gallery/LightboxImage.astro";
+import LightboxVideo from "../../components/gallery/LightboxVideo.astro";
 import ContentWarning from "../../components/posts/ContentWarning.astro";
 import FormattedDate from "../../components/FormattedDate.astro";
 import HiiveLabs from "../../components/custom/hiivelabs/HiiveLabs.astro";
@@ -11,8 +13,10 @@ import GithubLink from "../../components/links/GithubLink.astro";
 import EmbeddedYouTube from "../../components/EmbeddedYouTube.astro";
 export const components = {
     GalleryImage,
+    // GalleryVideo,
     LightboxGallery,
     LightboxImage,
+    LightboxVideo,
     ContentWarning,
     FormattedDate,
     SteamLink,
