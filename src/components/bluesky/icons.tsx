d6e72c55a28f6703a23277f999d8b97f481d6bd5
hiive <a.rollings@hiive.net>
2025-11-13 10:43:02 -0500

    Bluesky integration improvements

--- src/components/bluesky/icons.tsx
+++ src/components/bluesky/icons.tsx
@@ -3,6 +3,8 @@ import vsCommentSvg from "../../assets/images/shared/bluesky/vs-comment.svg?raw"
 import vsEmptyHeartSvg from "../../assets/images/shared/bluesky/vs-empty-heart.svg?raw";
 import vsFilledHeartSvg from "../../assets/images/shared/bluesky/vs-filled-heart.svg?raw";
 import vsLinkSvg from "../../assets/images/shared/bluesky/vs-link.svg?raw";
+import blueskyLinkSvg from "../../assets/images/shared/bluesky/bluesky.svg?raw";
+import externalLinkSvg from "../../assets/images/shared/links/-external-link.svg?raw";
 
 type IconProps = JSX.HTMLAttributes<HTMLSpanElement>;
 
@@ -21,3 +23,12 @@ export const VsFilledHeart: Component<IconProps> = (props) => (
 export const VsLink: Component<IconProps> = (props) => (
   <span innerHTML={vsLinkSvg} style={{ "pointer-events": "none", display: "inline-block" }} {...props} />
 );
+
+export const BlueskyLink: Component<IconProps> = (props) => (
+    <span innerHTML={blueskyLinkSvg} style={{ "pointer-events": "none", display: "inline-block" }} {...props} />
+);
+
+export const ExternalLink: Component<IconProps> = (props) => (
+    <span innerHTML={externalLinkSvg} style={{ "pointer-events": "none", display: "inline-block" }} {...props} />
+);
+
